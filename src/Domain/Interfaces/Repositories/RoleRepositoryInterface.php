0749f46e4c7f76d5fbdb7003d67eee360ebda03a
Vitaliy <theyamshikov@yandex.ru>
2021-01-10 13:03:14 +0600

    upd

--- src/Domain/Interfaces/Repositories/RoleRepositoryInterface.php
+++ src/Domain/Interfaces/Repositories/RoleRepositoryInterface.php
@@ -2,9 +2,12 @@
 
 namespace ZnBundle\Rbac\Domain\Interfaces\Repositories;
 
+use ZnBundle\Rbac\Domain\Entities\RoleEntity;
 use ZnCore\Domain\Interfaces\Repository\CrudRepositoryInterface;
+use ZnCore\Domain\Libs\Query;
 
 interface RoleRepositoryInterface extends ItemRepositoryInterface
 {
 
+    public function oneByName(string $name, Query $query = null): RoleEntity;
 }
--- src/Domain/Repositories/Eloquent/RoleRepository.php
+++ src/Domain/Repositories/Eloquent/RoleRepository.php
@@ -3,9 +3,11 @@
 namespace ZnBundle\Rbac\Domain\Repositories\Eloquent;
 
 use ZnBundle\Rbac\Domain\Entities\ItemEntity;
+use ZnBundle\Rbac\Domain\Entities\Role;
 use ZnBundle\Rbac\Domain\Entities\RoleEntity;
 use ZnBundle\Rbac\Domain\Interfaces\Repositories\RoleRepositoryInterface;
 use ZnCore\Domain\Entities\Query\Where;
+use ZnCore\Domain\Interfaces\Entity\EntityIdInterface;
 use ZnCore\Domain\Libs\Query;
 
 class RoleRepository extends ItemRepository implements RoleRepositoryInterface
@@ -21,5 +23,12 @@ class RoleRepository extends ItemRepository implements RoleRepositoryInterface
         $query = parent::forgeQuery($query);
         $query->whereNew(new Where('type', ItemEntity::TYPE_ROLE));
         return $query;
+    }
+
+    public function oneByName(string $name, Query $query = null): RoleEntity
+    {
+        $query = $this->forgeQuery($query);
+        $query->whereNew(new Where('name', $name));
+        return parent::one($query);
     }
 }
